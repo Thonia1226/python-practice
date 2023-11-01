770ce9e6f3f85f13c8f1d4eb7f74716b33a9df9b
Thonia1226 <97959510+Thonia1226@users.noreply.github.com>
2023-11-01 16:56:02 +0100

    number increment

---
+++ numberincrease.py
@@ -0,0 +1,11 @@
+#number increament using functions
+
+def numberincrease():
+    i=0
+    while i<5:
+        i+=1
+        print(i)
+        
+ #now call the funcrion       
+numberincrease()        
+        
