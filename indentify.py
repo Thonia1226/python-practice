c501e2ba07cead246a18bb8d0d2e13cbadd656ac
Thonia1226 <97959510+Thonia1226@users.noreply.github.com>
2023-11-01 16:52:36 +0100

    confirm if an item exist and print it out

---
+++ indentify.py
@@ -0,0 +1,7 @@
+#identify an item in a list
+
+list=["orange","orange","mango","pear","grape"]
+# creating a loop to check the list
+for items in list:
+    if items == "orange":
+        print (items)
